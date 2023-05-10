dd778b5b549bcde72937d49e20b9895a0e88d8a5
Rahul Gupta <rahulgtst@gmail.com>
2023-05-10 21:06:28 +0530

    Edit header css and remove one project from project data

--- data/projectData.js
+++ data/projectData.js
@@ -7,14 +7,6 @@ const projectData = [
     tools: ["react.js", "tailwind css"],
     image: "./src/assets/projectImages/Todo-app.png",
   },
-  {
-    id: 2,
-    name: "Tic Tac Toe",
-    githubLink: "",
-    websiteLink: "",
-    tools: ["HTML", "CSS", "JavaScript"],
-    image: "./src/assets/projectImages/Tic-tac-toe.jpg",
-  },
 ];
 
 export default projectData;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,53 +1,109 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import menu from "../assets/menu.png";
 import Logo from "./Logo";
 import { Link } from "react-scroll";
 
 const Header = () => {
   const [navigationOn, setNavigationOn] = useState(false);
+
   const handleNavigationVisibility = (e) => {
     e.stopPropagation();
-    navigationOn ? setNavigationOn(false) : setNavigationOn(true);
-    console.log(navigationOn);
+    setNavigationOn(!navigationOn);
   };
-  useEffect(() => {
-    const Navigation = document.getElementById("Navigation");
-    Navigation.style.display = navigationOn ? "block" : "none";
-  }, [navigationOn]);
+
   return (
     <header className="h-[60px] w-full lg:w-[99vw] px-5 sm:px-[40px] lg:px-[120px] py-[10px] bg-white border border-b-2 flex justify-between items-center fixed">
       <Logo />
-      <ul
+
+      <nav className="hidden md:block">
+        <ul className="flex gap-5 text-[14px] bg-white">
+          <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
+            <Link to="Home" spy={true} smooth={true} offset={50} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
+            <Link
+              to="About"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              About
+            </Link>
+          </li>
+          <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
+            <Link
+              to="Tools"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              Tools
+            </Link>
+          </li>
+          <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
+            <Link
+              to="Project"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              Projects
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <nav
         id="Navigation"
-        className="border fixed top-[60px] right-0 w-[100px] h-[150px] md:static md:top-0 flex md:gap-5 text-[14px] bg-white"
+        className={`md:hidden ${navigationOn ? "block" : "hidden"}`}
       >
-        <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
-          <Link to="Home" spy={true} smooth={true} offset={50} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
-          <Link to="About" spy={true} smooth={true} offset={50} duration={500}>
-            About
-          </Link>
-        </li>
-        <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
-          <Link to="Tools" spy={true} smooth={true} offset={50} duration={500}>
-            Tools
-          </Link>
-        </li>
-        <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
-          <Link
-            to="Project"
-            spy={true}
-            smooth={true}
-            offset={50}
-            duration={500}
-          >
-            Projects
-          </Link>
-        </li>
-      </ul>
+        <ul className="border fixed top-[60px] right-5 w-[100px] h-[220px] flex flex-col gap-5 text-[14px] bg-white">
+          <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
+            <Link to="Home" spy={true} smooth={true} offset={50} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
+            <Link
+              to="About"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              About
+            </Link>
+          </li>
+          <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
+            <Link
+              to="Tools"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              Tools
+            </Link>
+          </li>
+          <li className="p-2 rounded-lg hover:text-[#4831D4] hover:cursor-pointer">
+            <Link
+              to="Project"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              Projects
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
       <img
         src={menu}
         alt="menu"
